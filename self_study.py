b9bc84a451ad5ef8ceeb4013f27831ac8e4eea1a
Tim Hug <tim.j.hug@gmail.com>
2021-04-27 17:39:42 +0200

    added data to repository, set up a functions file, import of functions and data included in self_study.py

--- self_study.py
+++ self_study.py
@@ -1,7 +1,39 @@
 """
-Microeconometrics: PC1.
+Microeconometrics: Self Study.
+
+Group Assignment:
+    
+Alec Eisenkolb, Chung-Shun Man, Nicolas Greber, Tim Hug
 
 Spring Semester 2021.
 
 University of St. Gallen.
 """
+
+# import modules
+import os
+import numpy as np
+import pandas as pd
+
+# set working directory to current directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# import own functions
+import self_study_functions as ssf
+
+# set the seep for replicability
+np.random.seed(270421)
+
+# define name of data
+DATA = 'data_group_1.csv'
+
+# load in the data
+data = pd.read_csv(DATA)
+
+
+# --------------------------------------------------------------------------- #
+
+# Data Cleaning
+
+
+
---
+++ self_study_functions.py
@@ -0,0 +1,20 @@
+"""
+Microeconometrics: Self Study Functions.
+
+Group Assignment:
+    
+Alec Eisenkolb, Chung-Shun Man, Nicolas Greber, Tim Hug
+
+Spring Semester 2021.
+
+University of St. Gallen.
+"""
+
+# import modules
+
+
+
+
+# --------------------------------------------------------------------------- #
+
+# first function
